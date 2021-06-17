0f9199b8bdf8a2ef2f0aa440d01041cdae7c99ff
weeer <kzs1845@gmail.com>
2021-06-17 11:22:40 +0300

    add infinite scrolling

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const postContainer = document.getElementById('posts-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
-let limit = 3;
+let limit = 5;
 let page = 1;
 
 async function getPosts() {
@@ -31,4 +31,26 @@ async function showPosts() {
 	})
 }
 
+function showLoading() {
+	loading.classList.add('show');
+
+	setTimeout(() => {
+		loading.classList.remove('show');
+
+		setTimeout(() => {
+			page++;
+			showPosts();
+		}, 300);
+
+	}, 1000)
+}
+
 showPosts()
+
+window.addEventListener('scroll', () => {
+	const { scrollTop, scrollHeight, clientHeight} = document.documentElement;
+
+	if(scrollTop + clientHeight >= scrollHeight - 5) {
+		showLoading();
+	}
+})
